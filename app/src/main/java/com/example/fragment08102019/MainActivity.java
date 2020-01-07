bb228f1687dcb73bd7b6bb329d5aa4dc3cbc523f
phatdroid94 <phatdroid94@gmail.com>
2020-01-07 20:19:02 +0700

    check count fragment

--- app/src/main/java/com/example/fragment08102019/MainActivity.java
+++ app/src/main/java/com/example/fragment08102019/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.fragment08102019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.add(R.id.linearLayoutContainer,androidFragment);
+        fragmentTransaction.commit();
+
+        runOnUiThread(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if(fragmentManager.executePendingTransactions()){
+                    Log.d("BBB","Xu ly xong");
+                }else{
+                    Log.d("BBB","Chua xong");
+                    this.run();
+                }
+            }
+        }));
 
     }
 }
